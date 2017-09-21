3ba7730621a26cd2763841f84d822a0abde61a6f
davechurilla <churilla@gmail.com>
2017-09-20 20:26:14 -0700

    main page complete, ready to set up modal overlay

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,44 +1,37 @@
 document.addEventListener('DOMContentLoaded', function(event) { 
 
 var heroGalleryDb = [
-    {img:'http://placekitten.com/g/750/500',name:'Zeus',title:'Being cute',age:'1 month'},
-    {img:'http://placekitten.com/g/600/400',name:'Lola',title:'Purring',age:'3 months'},
-    {img:'http://placekitten.com/g/450/300',name:'Jasper',title:'Sleeping',age:'1.5 months'},
-    {img:'http://placekitten.com/g/800/533',name:'Drake',title:'Eating',age:'3 months'},
-    {img:'http://placekitten.com/g/900/600',name:'Luna',title:'Catching pesky mice',age:'3 weeks'}
+    {img:'http://placekitten.com/g/750/500',name:'Zeus',title:'Being cute',age:'1 month',article:'<h2>Check out this guy!</h2><p>Aliquam mauris dolor, posuere at augue vel, fringilla semper erat. Ut vehicula malesuada cursus. Donec at urna libero. Nulla sodales bibendum vulputate. Curabitur erat mauris, aliquam in augue non, porta viverra nisi. Ut nulla sem, mattis ut hendrerit quis, condimentum eget ex. Curabitur sed justo quis leo bibendum porta. Praesent sed malesuada ipsum. Aliquam malesuada purus augue, et tincidunt lectus placerat et. Nulla facilisis lorem sit amet ullamcorper tempus. Curabitur egestas aliquam suscipit. Nunc facilisis rhoncus leo sit amet convallis.</p>'},
+    {img:'http://placekitten.com/g/600/400',name:'Lola',title:'Purring',age:'3 months',article:'<h2>Neat!</h2><p>Aliquam mauris dolor, posuere at augue vel, fringilla semper erat. Ut vehicula malesuada cursus. Donec at urna libero. Nulla sodales bibendum vulputate. Curabitur erat mauris, aliquam in augue non, porta viverra nisi. Ut nulla sem, mattis ut hendrerit quis, condimentum eget ex. Curabitur sed justo quis leo bibendum porta. Praesent sed malesuada ipsum. Aliquam malesuada purus augue, et tincidunt lectus placerat et. Nulla facilisis lorem sit amet ullamcorper tempus. Curabitur egestas aliquam suscipit. Nunc facilisis rhoncus leo sit amet convallis.</p>'},
+    {img:'http://placekitten.com/g/450/300',name:'Jasper',title:'Sleeping',age:'1.5 months',article:'<h2>Meow, meow, meow...</h2><p>Aliquam mauris dolor, posuere at augue vel, fringilla semper erat. Ut vehicula malesuada cursus. Donec at urna libero. Nulla sodales bibendum vulputate. Curabitur erat mauris, aliquam in augue non, porta viverra nisi. Ut nulla sem, mattis ut hendrerit quis, condimentum eget ex. Curabitur sed justo quis leo bibendum porta. Praesent sed malesuada ipsum. Aliquam malesuada purus augue, et tincidunt lectus placerat et. Nulla facilisis lorem sit amet ullamcorper tempus. Curabitur egestas aliquam suscipit. Nunc facilisis rhoncus leo sit amet convallis.</p>'},
+    {img:'http://placekitten.com/g/800/533',name:'Drake',title:'Eating',age:'3 months',article:'<h2>ROOOOAAAR!</h2><p>Aliquam mauris dolor, posuere at augue vel, fringilla semper erat. Ut vehicula malesuada cursus. Donec at urna libero. Nulla sodales bibendum vulputate. Curabitur erat mauris, aliquam in augue non, porta viverra nisi. Ut nulla sem, mattis ut hendrerit quis, condimentum eget ex. Curabitur sed justo quis leo bibendum porta. Praesent sed malesuada ipsum. Aliquam malesuada purus augue, et tincidunt lectus placerat et. Nulla facilisis lorem sit amet ullamcorper tempus. Curabitur egestas aliquam suscipit. Nunc facilisis rhoncus leo sit amet convallis.</p>'},
+    {img:'http://placekitten.com/g/900/600',name:'Luna',title:'Catching pesky mice',age:'3 weeks',article:'<h2>Not me! Talk to the dog...</h2><p>Aliquam mauris dolor, posuere at augue vel, fringilla semper erat. Ut vehicula malesuada cursus. Donec at urna libero. Nulla sodales bibendum vulputate. Curabitur erat mauris, aliquam in augue non, porta viverra nisi. Ut nulla sem, mattis ut hendrerit quis, condimentum eget ex. Curabitur sed justo quis leo bibendum porta. Praesent sed malesuada ipsum. Aliquam malesuada purus augue, et tincidunt lectus placerat et. Nulla facilisis lorem sit amet ullamcorper tempus. Curabitur egestas aliquam suscipit. Nunc facilisis rhoncus leo sit amet convallis.</p>'}
 ];
 
 function init() {
 	currentImg();
 	heroNext();
 	heroPrev();
-
-	var img1 = document.getElementsByClassName('slide-img')[0];
-	img1.onload = function() {
-    console.log('Image 1 ready to append');
-    // var slideEl = document.getElementsByClassName('slide')[0];
-	var slideImg = document.getElementsByClassName('slide-img')[0];
-	var slideHeight= slideImg.offsetHeight;
-	slideImg.style.height = slideHeight + 'px';	    	
-	};
+	// setImgHeight();
 }
 
 function currentImg(){
-    var parent = document.querySelector('#slideContainer');
+    var heroParent = document.querySelector('#slideContainer .slide');
+    var copyParent = document.querySelector('#copyContainer .body-copy');
     var template = '';
-        template += '<li class="slide">';
-        template += '<div class="img-title">';
-        template += '	<h4><strong>Name:</strong> '+ heroGalleryDb[0].name +'</h4>';
-        template += '	<p><strong>Occupation:</strong> '+ heroGalleryDb[0].title + ', ';
-        template += '	<strong>Age:</strong> '+ heroGalleryDb[0].age +'</p>';
-        template += '</div>';
-        template += '<img src="'+ heroGalleryDb[0].img +'" class="slide-img" width="100%" alt="'+ heroGalleryDb[0].name +'" /></li>';
-    parent.innerHTML = '';
-    parent.insertAdjacentHTML('afterbegin', template);
-
-    // var navArrow = document.getElementsByClassName('nav_arrow');
-    // navArrow[0].style.display = 'block';
-    // navArrow[1].style.display = 'block';
+        template += '	<div class="img-title">';
+        template += '		<h4><strong>Name:</strong> '+ heroGalleryDb[0].name +'</h4>';
+        template += '		<p><strong>Occupation:</strong> '+ heroGalleryDb[0].title + ' <br />';
+        template += '		<strong>Age:</strong> '+ heroGalleryDb[0].age +'</p>';
+        template += '	</div>';
+        template += '	<div class="slide-img-container"><img src="'+ heroGalleryDb[0].img +'" class="slide-img" width="100%" alt="'+ heroGalleryDb[0].name +'" /></div>'
+	var copyTemplate = ''; 
+		copyTemplate += heroGalleryDb[0].article;       
+	heroParent.innerHTML = '';
+    copyParent.innerHTML = '';
+    heroParent.insertAdjacentHTML('afterbegin', template);
+    copyParent.insertAdjacentHTML('afterbegin', copyTemplate);
+    setImgHeight();
 };
 
 function heroNext(){
@@ -73,6 +66,42 @@ function heroPrev(){
     });
 };
 
+ function preloadImages(array) {
+    if (!preloadImages.list) {
+        preloadImages.list = [];
+    }
+    var list = preloadImages.list;
+    for (var i = 0; i < array.length; i++) {
+        var img = new Image();
+        img.onload = function() {
+            var index = list.indexOf(this);
+            if (index !== -1) {
+                // remove image from the array once it's loaded
+                // for memory consumption reasons
+                list.splice(index, 1);
+            }
+        }
+        list.push(img);
+        img.src = array[i];
+    }
+}
+
+preloadImages(["http://placekitten.com/g/750/500", "http://placekitten.com/g/600/400", "http://placekitten.com/g/450/300", "http://placekitten.com/g/800/533", "http://placekitten.com/g/900/600"]);
+
 init();
-// document.getElementsByClassName('slide-img')[0].offsetHeight;
 });
+
+function setImgHeight() {
+	var img1 = document.getElementsByClassName('slide-img')[0];
+	img1.onload = function() {
+	    // console.log('Image 1 ready to append');	
+        var slide = document.getElementsByClassName('slide')[0];
+        var slideImgTitle = document.getElementsByClassName('img-title')[0];
+        var slideImg = document.getElementsByClassName('slide-img')[0];
+        var slideImgTitleHeight = slideImgTitle.offsetHeight;
+        var slideImgHeight= slideImg.offsetHeight;
+        var slideHeight = parseInt(slideImgTitleHeight) + parseInt(slideImgHeight);
+        slide.style.height = slideHeight + 'px';
+	};	
+}
+
